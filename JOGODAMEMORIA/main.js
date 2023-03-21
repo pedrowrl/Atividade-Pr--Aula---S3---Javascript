a5243b404702d2aa44f920e080aec86aaa56f3bb
pedrowrl <93289260+pedrowrl@users.noreply.github.com>
2023-03-21 15:24:39 -0300

    Add files via upload

---
+++ JOGODAMEMORIA/main.js
@@ -0,0 +1,69 @@
+const cardBoard = document.querySelector("#cardboard");
+const images = [
+    "Bart.jpg",
+    "Homer.jpg",
+    "Lisa.jpg",
+    "Maggie.jpg",
+    "Marge.jpg",
+    "Moe.jpg"
+];
+
+let cardHTML = "";
+
+images.forEach(img => {
+    cardHTML += `
+    <div class="memory-card" data-card="${img}">
+        <img class="front-face" src="img/${img}">
+        <img class="back-face" src="img/simpsons.jpg">
+    </div>
+    `;
+});
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
+/** Fim da renderização HTML */
+
+const cards = document.querySelectorAll(".memory-card");
+let firstCard, secondCard;
+let lockCards = false;
+
+function flipCard() {
+  if (lockCards) return false;
+  this.classList.add("flip");
+
+  if (!firstCard) {
+    firstCard = this;
+    return false;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+  !isMatch ? unFlipCards() : resetCards(isMatch);
+}
+
+function unFlipCards() {
+  lockCards = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetCards();
+  }, 1000);
+}
+
+function resetCards(isMatch = false) {
+  if (isMatch) {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  }
+
+  [firstCard, secondCard, lockCards] = [null, null, false];
+}
+
+cards.forEach(card => card.addEventListener("click", flipCard));
